d1d3b80c54edbbcf78085d4c3434f8fc614e3fb9
manu_aatitya <manu@harmonicbionics.com>
2020-06-03 00:58:50 +0530

    Main Function Updated

--- main.py
+++ main.py
@@ -11,16 +11,28 @@ from cfd_simulation import simulation
 # Main Driver Function for the application
 def main():
     # Get the number of elements input from the user
-    number_elements = int(input('Enter the number of nodes needed in the domain \n'))
+    number_elements = int(input('Enter the number of nodes needed in the domain \t'))
     
     # Get the total time (seconds) input from the user
-    evolution_time = int(input('Enter the time (s) required for the evolution simulation \n'))
+    evolution_time = int(input('Enter the time (s) required for the evolution simulation \t'))
 
     timesteps = evolution_time/200
 
-    tolerance = float(input('Enter the tolerance required for the Gauss Siedal Convergence'))
+    tolerance = float(input('Enter the tolerance required for the Gauss Siedal Convergence \t'))
 
-    simulation1 = simulation(number_elements,evolution_time,timesteps,tolerance)
+    D = float(input("Enter the D value in the unsteady diffusion equation \t"))
+
+    initial_concentration = float(input("Enter the initial concentration \t"))
+
+    # Simulation start and functions
+    simulation1 = simulation(number_elements,evolution_time,timesteps,tolerance,D,initial_concentration)
+    print("Simulation parameters initialised")
+    simulation1.generate_mass_matrix()
+    print("Mass Matrix M generated")
+    simulation1.generate_stiffness_matrix()
+    print("Stiffness Matrix K generated")
+    simulation1.generate_solution_of_equations()
+    print("Concentration values at each grid point calculated")
 
 
 if __name__ == "__main__":
