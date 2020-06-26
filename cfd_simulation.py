f48fb3a1c379ce1535e5ef27392af00be8dbdd50
manu_aatitya <manu@harmonicbionics.com>
2020-06-26 16:35:40 +0530

    Plot code Added and Final Matrix calculation function modified

--- cfd_simulation.py
+++ cfd_simulation.py
@@ -57,7 +57,7 @@ class simulation:
 
     # Function to check if the solution is within the specified limits
     def check_x_solution_within_limit(self,temp_x,prev_x):
-        for i in range (0,self.nodes):
+        for i in range (0,len(prev_x)):
             if(not(abs(temp_x[i] - prev_x[i]) <= self.tolerance)):
                 return False
         return True
@@ -65,10 +65,10 @@ class simulation:
     # Gauss Seidal Method to solve the system AX = B
     # X is the initial trial solution
     def gauss_seidal(self,A,B,X):
-        for i in range(0,self.nodes):
+        for i in range(0,len(A)):
             # Temporary variable to store intermediate variables
             temp = B[i]
-            for j in range(0,self.nodes):
+            for j in range(0,len(A)):
                 if(i != j):
                     temp -= A[i][j] * X[j] # temp_x_solution[j]
             # Update value for the next iteration
@@ -81,9 +81,24 @@ class simulation:
 
     # Find the concentration at each grid point
     def generate_solution_of_equations(self):
+        self.M = self.M[1 : self.nodes - 1, 1 : self.nodes - 1]
+        self.K = self.K[1 : self.nodes - 1, 1 : self.nodes - 1]
+        print("Printing K ",self.K.shape)
         for i in range(1,200):
-            temp = self.solution_matrix[i - 1].transpose()
+            # temp = self.solution_matrix[i - 1].transpose()
+            temp = self.solution_matrix[i-1][1 : self.nodes - 1].transpose()
             B = self.M @ temp - self.dt * (self.K @ temp)
-            self.prev_X = np.zeros(self.nodes)
-            self.solution_matrix[i] = self.gauss_seidal(self.M,B,np.zeros(self.nodes))
+            self.prev_X = np.zeros(len(self.M))
+            self.solution_matrix[i][1 : self.nodes - 1] = self.gauss_seidal(self.M,B,np.zeros(len(self.M)))
+    
+    # Plot the values in a graph
+    def plot_concentration(self):
+        x_axis = np.linspace(0,1,self.nodes)
+        plt.plot(x_axis,self.solution_matrix[5],label = "t =5")
+
+        plt.plot(x_axis,self.solution_matrix[50], label = "t = 50")
+
+        plt.plot(x_axis,self.solution_matrix[80], label = "t = 80")
+        plt.legend()
+        plt.show()
     
